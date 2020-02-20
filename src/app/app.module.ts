83450586f699ece85c42882a2b14a807c83e7586
Yura Husak <yura.husak@juriba.com>
2020-02-21 00:34:33 +0200

    Base config of tempates, styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { SelectorComponent } from './components/selector/selector.component';
+import { OrganizerComponent } from './components/organizer/organizer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    SelectorComponent,
+    OrganizerComponent
   ],
   imports: [
     BrowserModule,
