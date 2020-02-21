0d7e84f10395e2f5d7480a6c27104125c2a3720b
Yura Husak <yura.husak@juriba.com>
2020-02-21 15:44:15 +0200

    Added pug files and update scripts

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
--- src/app/components/organizer/organizer.component.ts
+++ src/app/components/organizer/organizer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+    // tslint:disable-next-line: component-selector
   selector: 'organizer',
   templateUrl: './organizer.component.html',
   styleUrls: ['./organizer.component.scss']
